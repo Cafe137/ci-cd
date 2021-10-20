dee8e49b9cc0f96d3c16ad908d76e5d68592403a
Cafe137 <77121044+Cafe137@users.noreply.github.com>
2021-10-20 08:51:48 +0200

    feat: silently catch all errors (#1)

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-console.log(require('cafe-utility').Strings.randomHex(parseInt(process.argv[2], 10) || 0))
+try {
+    console.log(require('cafe-utility').Strings.randomHex(parseInt(process.argv[2], 10) || 0))
+} catch {}
